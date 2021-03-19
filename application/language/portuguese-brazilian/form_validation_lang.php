909c178f3654517bfab21465dd9efec4ec19452c
Antonio Bevilaqua <toni_bevila@hotmail.com>
2021-03-19 12:06:10 -0300

    correção linguagem

--- application/language/portuguese-brazilian/form_validation_lang.php
+++ application/language/portuguese-brazilian/form_validation_lang.php
@@ -39,4 +39,4 @@ $lang['form_validation_less_than_equal_to'] = 'O campo {field} deve conter um n�
 $lang['form_validation_greater_than'] = 'O campo {field} deve conter um número maior que {param}.';
 $lang['form_validation_greater_than_equal_to'] = 'O campo {field} deve conter um número maior ou igual que {param}.';
 $lang['form_validation_error_message_not_set'] = 'Não existe uma mensagem de erro para o campo com o nome {field}.';
-$lang['form_validation_in_list'] = 'O campo {field} deve ser um de: {param}.';
+$lang['form_validation_in_list'] = 'O campo {field} deve ser um de: {param}.'; ?>
